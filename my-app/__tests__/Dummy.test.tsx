8de7e487543b14ac6c697c18ac54f996b9717b1c
Deen John <dj.itbhu@gmail.com>
2024-05-16 17:56:51 +0000

    adding test

---
+++ my-app/__tests__/Dummy.test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+
+
+test("Test dummy", () => {
+
+  expect(
+    1 + 1,
+  ).toBe(2)
+
+
+});
